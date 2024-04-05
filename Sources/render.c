1db55a6c6c39cc11e66c3f289373bcbcc3675391
Lochane <skeleton9111@gmail.com>
2024-04-05 19:40:24 +0200

    aaaaaaaaaaa

--- Sources/render.c
+++ Sources/render.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lsouquie <lsouquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 18:52:08 by lsouquie          #+#    #+#             */
-/*   Updated: 2024/03/23 15:10:45 by malancar         ###   ########.fr       */
+/*   Updated: 2024/04/05 19:38:53 by lsouquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,32 +30,44 @@ t_img	get_img(t_data *data)
 	}
 }
 
+int	check_calcul(int calcul, t_img img, t_data *data)
+{
+	if ((data->texture.pixel_x < 0 && data->texture.pixel_x >= data->img.width) \
+	|| (data->texture.pixel_y < 0 && data->texture.pixel_y >= data->img.height))
+		return (1);
+	if (calcul < 0)
+		return (1);
+	if (calcul > img.width * img.height)
+		return (1);
+	return (0);
+}
+
 int	render_texture(t_data *data, int height, int beg_wall, int end_wall)
 {
-	double	block_size;
-	double	ratio_x;
-	double	ratio_y;
-	t_img	img;
+	const double	block_size = end_wall - beg_wall;
+	double			ratio_x;
+	double			ratio_y;
+	int				calcul;
+	const t_img		img = get_img(data);
 
-	img = get_img(data);
 	if (data->ray.is_horizontal == 0)
 		ratio_x = data->ray.inter_y - (int)data->ray.inter_y;
 	else
 		ratio_x = data->ray.inter_x - (int)data->ray.inter_x;
 	data->texture.pixel_x = img.width * ratio_x;
-	block_size = end_wall - beg_wall;
 	ratio_y = (height - beg_wall) / block_size;
 	data->texture.pixel_y = (int)(img.height * ratio_y);
 	data->ray.dirx = cos(data->ray.angles[data->ray.ray_index] * (M_PI / 180));
 	data->ray.diry = sin(data->ray.angles[data->ray.ray_index] * (M_PI / 180));
-	if ((data->texture.pixel_x < 0 && data->texture.pixel_x >= data->img.width) \
-	|| (data->texture.pixel_y < 0 && data->texture.pixel_y >= data->img.height))
-		return (BLACK);
 	if (img.print == 0)
-		return (img.addr[((int)data->texture.pixel_y * img.width) - \
-			(int)data->texture.pixel_x]);
-	return (img.addr[(int)data->texture.pixel_x + \
-		(int)data->texture.pixel_y * img.width]);
+		calcul = ((int)data->texture.pixel_y * img.width) - \
+			(int)data->texture.pixel_x;
+	else
+		calcul = (int)data->texture.pixel_x + \
+			(int)data->texture.pixel_y * img.width;
+	if (check_calcul(calcul, img, data) != 0)
+		return (BLACK);
+	return (img.addr[calcul]);
 }
 
 void	render_wall(t_data *data, int width)
