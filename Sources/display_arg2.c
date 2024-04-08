873be8bafec80dfb2b447ad6d9a0929361925d12
Marine LANCAR <malancar@bess-f2r3s16.clusters.42paris.fr>
2024-04-08 20:47:48 +0200

    ok??????????????????????????????

--- Sources/display_arg2.c
+++ Sources/display_arg2.c
@@ -6,7 +6,7 @@
 /*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 18:14:26 by lsouquie          #+#    #+#             */
-/*   Updated: 2024/04/08 20:22:02 by malancar         ###   ########.fr       */
+/*   Updated: 2024/04/08 20:47:11 by malancar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	check_wrong_char(char **tmp, t_data *data)
 	{
 		if (ft_strcmp("NO", tmp[0]) != 0 && ft_strcmp("SO", tmp[0]) != 0 && \
 				ft_strcmp("EA", tmp[0]) != 0 && ft_strcmp("WE", tmp[0]) != 0 && \
-					ft_strcmp("F", tmp[0]) != 0 && ft_strcmp("C", tmp[0]) != 0 && \
+				ft_strcmp("F", tmp[0]) != 0 && ft_strcmp("C", tmp[0]) != 0 && \
 					ft_strcmp("\n", tmp[0]) != 0)
 		{
 			free_tab(tmp, found_size(tmp, 0), data, 0);
